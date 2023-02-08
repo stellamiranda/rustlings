c57e14d7d6e5ba5ab5ea582309269071468ef58f
Stella Miranda <stella.miranda@shopify.com>
2023-02-08 05:30:11 +0000

    using structs

---
+++ structs_example/target/debug/deps/structs_example-5c921d051e7dff9e.d
@@ -0,0 +1,5 @@
+/workspaces/rustlings/structs_example/target/debug/deps/structs_example-5c921d051e7dff9e: src/main.rs
+
+/workspaces/rustlings/structs_example/target/debug/deps/structs_example-5c921d051e7dff9e.d: src/main.rs
+
+src/main.rs:
---
+++ structs_example/target/debug/structs_example.d
@@ -0,0 +1,1 @@
+/workspaces/rustlings/structs_example/target/debug/structs_example: /workspaces/rustlings/structs_example/src/main.rs
